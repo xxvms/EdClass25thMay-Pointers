90fc7fe622c4e48c9d835bcf770ef4e5360acb22
Tom Breza <tom@pcservicegroup.co.uk>
2017-05-25 20:27:57 +0100

    further examples from class

--- EdClass25thMay-Pointers/EdClass25thMay-Pointers.cpp
+++ EdClass25thMay-Pointers/EdClass25thMay-Pointers.cpp
@@ -3,17 +3,60 @@
 
 #include <iostream>
 using namespace std;
+
+
+void dodo(int *);
+
 int main()
 {
-	int i;
-	int* ptr;
-	ptr = &i;
-	*ptr = 5;
-	cout << *ptr << endl;
+	int i = 3;
+
+	dodo(&i); // pass the address of i
+
+	cout << i;
 	cin.get();
 	return 0;
 }
 
+void dodo(int* ptri)		// the * here means IS A POINTER	
+{
+	(*ptri)++;
+}
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int arr[] = { 4,6,9 };
+//	cout << arr[0] << " " << &arr[0] << endl;
+//	cout << *arr << " " << arr << endl;
+//
+//	int* ptr;
+//
+//	ptr = arr;
+//
+//	cout << "PTR: " << ptr << endl;
+//	ptr++;
+//	cout << "PTR: " << ptr << endl;
+//	cout << "ptr: " << *ptr << endl;
+//
+//	cin.get();
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i;
+//	int* ptr;
+//	ptr = &i;
+//	*ptr = 5;
+//	cout << *ptr << endl;
+//	cin.get();
+//	return 0;
+//}
+//
 
 //#include <iostream>
 //using namespace std;
